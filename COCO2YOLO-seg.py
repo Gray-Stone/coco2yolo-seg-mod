51d902de70e0fbf0456c6bef583407d78793a569
leo-chen <S8O9R6@u.northwestern.edu>
2024-05-20 14:57:24 -0500

    Add bbox feature

--- COCO2YOLO-seg.py
+++ COCO2YOLO-seg.py
@@ -100,7 +100,7 @@ class YoloDataset():
             yaml.dump(yaml_dict , file )
 
 def convert_coco_to_yolo_segmentation(json_file:pathlib.Path, source_img_dir: pathlib.Path,
-                                      dest_folder: pathlib.Path):
+                                      dest_folder: pathlib.Path,action_bbox = False):
 
     # Load the JSON file
     with open(json_file, 'r') as file:
@@ -154,34 +154,27 @@ def convert_coco_to_yolo_segmentation(json_file:pathlib.Path, source_img_dir: pa
         # Moving the above loop here ensure linking only when segment exists.
         yolo_dataset.TryLinkImage(image_info)
 
-        # # This is a not used bbox feature.
-        # # Calculate the normalized center coordinates and width/height
-        # x_center = (bbox[0] + bbox[2] / 2) / image_info.width
-        # y_center = (bbox[1] + bbox[3] / 2) / image_info.height
-        # bbox_width = bbox[2] / image_info.width
-        # bbox_height = bbox[3] / image_info.height
+        if action_bbox:
+            # This is a not used bbox feature.
+            # Calculate the normalized center coordinates and width/height
+            x_center = (bbox[0] + bbox[2] / 2) / image_info.width
+            y_center = (bbox[1] + bbox[3] / 2) / image_info.height
+            bbox_width = bbox[2] / image_info.width
+            bbox_height = bbox[3] / image_info.height
+            # output layout class x_center y_center width height
+            yolo_annotation = f"{category_id} {x_center} {y_center} {bbox_width} {bbox_height}"
 
-        # Find the image filename from the COCO data
-        # TODO optimize later.
-        # for image in coco_data['images']:
-        #     if image['id'] == image_id:
-        #         image_filename = os.path.basename(image['file_name'])
-        #         image_filename = os.path.splitext(image_filename)[
-        #             0]  # Removing the extension. (In our case, it is the .jpg or .png part.)
-        #         image_width = image['width']
-        #         image_height = image['height']
-        #         break
+        else:
+            # Convert COCO segmentation to YOLO segmentation format
+            yolo_segmentation = [
+                f"{(x) / image_info.width:.5f} {(y) / image_info.height:.5f}"
+                for x, y in zip(segmentation[0][::2], segmentation[0][1::2])
+            ]
+            #yolo_segmentation.append(f"{(segmentation[0][0]) / image_width:.5f} {(segmentation[0][1]) / image_height:.5f}")
+            yolo_segmentation = ' '.join(yolo_segmentation)
 
-        # Convert COCO segmentation to YOLO segmentation format
-        yolo_segmentation = [
-            f"{(x) / image_info.width:.5f} {(y) / image_info.height:.5f}"
-            for x, y in zip(segmentation[0][::2], segmentation[0][1::2])
-        ]
-        #yolo_segmentation.append(f"{(segmentation[0][0]) / image_width:.5f} {(segmentation[0][1]) / image_height:.5f}")
-        yolo_segmentation = ' '.join(yolo_segmentation)
-
-        # Generate the YOLO segmentation annotation line
-        yolo_annotation = f"{category_id} {yolo_segmentation}"
+            # Generate the YOLO segmentation annotation line
+            yolo_annotation = f"{category_id} {yolo_segmentation}"
 
         # Also record this category line.
         # Save the YOLO segmentation annotation in a file
@@ -198,6 +191,7 @@ if __name__ == "__main__":
     parser.add_argument("json_file", type=pathlib.Path, help="path to coco json file")
     parser.add_argument("image_dir", default='./data', type=pathlib.Path, help="output dir")
     parser.add_argument("output_dir", default='./Yolo-conv', type=pathlib.Path, help="output dir")
+    parser.add_argument("--bbox" , default=False , action="store_true" , help="Default doing segmentation, if set, then do bounding box.")
 
     # parser.add_argument("--force_id_pair")
 
@@ -209,4 +203,4 @@ if __name__ == "__main__":
     if not image_source.exists():
         raise ValueError(f"Image dir {image_source} doesn't exists")
 
-    convert_coco_to_yolo_segmentation(json_file.resolve(), image_source, out_dir)
+    convert_coco_to_yolo_segmentation(json_file.resolve(), image_source, out_dir , action_bbox = args.bbox)
